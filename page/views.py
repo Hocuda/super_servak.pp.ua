5b5694d11fbc28f87f7c1f5853f592c4791e2a04
Hocuda <>
2024-08-16 23:31:37 +0300

    Caching random images generator for index

--- page/views.py
+++ page/views.py
@@ -1,14 +1,28 @@
+import time
 from random import choice
 from django.shortcuts import render
 from .models import Stuff_card
 
+cached_context = None
+last_time_update = 0
+
+def refrash_index():
+    global cached_context, last_time_update
+    current_time = time.time()
+
+    if current_time - last_time_update > 60 or current_time == None:
+        objects = Stuff_card.objects.all()
+        cached_context = {'stuff1': choice(objects),
+                    'stuff2': choice(objects),
+                    'stuff3': choice(objects),
+                    'stuff4': choice(objects),}
+        last_time_update = current_time
+        
+    return cached_context
+
 def index(request):
     """Главная страница"""
-    objects = Stuff_card.objects.all()
-    context = {'stuff1': choice(objects),
-                'stuff2': choice(objects),
-                'stuff3': choice(objects),
-                'stuff4': choice(objects),}
+    context = refrash_index()
     return render(request, 'page/index.html', context)
 
 
