08783e45b6206748356d2445ccc6b732f689f419
Hocuda <baam6t@gmail.com>
2024-08-26 15:11:41 +0300

    removed duplicate products from index; Linux and Linus Torvalds are not displayed

--- page/views.py
+++ page/views.py
@@ -3,19 +3,23 @@ from random import choice
 from django.shortcuts import render
 from .models import Stuff_card
 
-cached_context = None
+cached_context = {}
 last_time_update = 0
 
 def refrash_index():
     global cached_context, last_time_update
     current_time = time.time()
 
-    if current_time - last_time_update > 60 or current_time == None:
+    if current_time - last_time_update > 0.1 or current_time == None:
         objects = Stuff_card.objects.all()
-        cached_context = {'stuff1': choice(objects),
-                    'stuff2': choice(objects),
-                    'stuff3': choice(objects),
-                    'stuff4': choice(objects),}
+        select_item = set()
+        for i in range(1, 5):
+            for_var = choice(objects)
+            while for_var in select_item or for_var == objects.get(id=2) or for_var == objects.get(id=3):
+                for_var = choice(objects)
+            select_item.add(for_var)
+            cached_context[f'stuff{i}'] = for_var
+
         last_time_update = current_time
         
     return cached_context
