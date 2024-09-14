753fed96cc5a84b3eb49cdb7430d34a20f5b4256
Hocuda <baam6t@gmail.com>
2024-09-14 14:06:20 +0300

    remove forms.py, added base2.html

--- page/forms.py
+++
@@ -1,7 +0,0 @@
-# в файле forms.py вашего приложения
-
-from django import forms
-
-class MyForm(forms.Form):
-    my_choices = [(1, 'Билл Гейтс'), (2, 'Линукс Торвальдс'), (3, 'Марк Цукерберг')]
-    my_field = forms.ChoiceField(choices=my_choices, widget=forms.Select(attrs={'min': '1'}))
